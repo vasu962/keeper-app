bd24b691e4312b8ab5e0922fd5e5804111a7c719
vasuu_kk <vasukumar962@gmail.com>
2023-09-14 23:24:54 +0530

    Updates

---
+++ precache-manifest.62c7d881e09d899ab1538a18fe633264.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9641db5a14f91e9ed77e3bd4c9d418f",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "726162a667de35633a5f",
+    "url": "/keeper-app/static/js/2.15e549da.chunk.js"
+  },
+  {
+    "revision": "c8c8f8e9691773125c8a",
+    "url": "/keeper-app/static/js/main.01c2ca00.chunk.js"
+  },
+  {
+    "revision": "7497e34bc9a978950d71",
+    "url": "/keeper-app/static/js/runtime-main.5f33c95a.js"
+  }
+]);
